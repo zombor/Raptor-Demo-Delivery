12f8d528e1c6d6234b65c29664533a645dd14408
Jeremy Bush <contractfrombelow@gmail.com>
2012-06-07 20:33:46 -0500

    add user repository, model and specs

---
+++ lib/models/user.rb
@@ -0,0 +1,21 @@
+module MyKissList
+  module Records
+    class User
+      attr_reader :id, :email, :password
+
+      def initialize(attributes={})
+        attributes ||= {}
+        attributes.each_pair do |attr, value|
+          setter = "#{attr}=".to_sym
+          if self.respond_to?(setter)
+            self.send setter, value
+          end
+        end
+      end
+
+      protected
+
+      attr_writer :id, :email, :password
+    end
+  end
+end
